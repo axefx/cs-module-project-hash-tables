7603ccad8fa98ffeb101f6018dfd6b7f393db91c
Axel <axacorro@gmail.com>
2020-07-09 15:48:55 -0700

    implemented word map from all words in input.txt

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -6,6 +6,16 @@ with open("input.txt") as f:
 
 # TODO: analyze which words can follow other words
 # Your code here
+words_list = words.split()
+word_pairs = []
+for i in range(len(words_list) - 1):
+    word_pairs.append((words_list[i],words_list[i + 1]))
+word_map = {}
+for w1,w2 in word_pairs:
+    if w1 in word_map:
+        word_map[w1].append(w2)
+    else:
+        word_map[w1] = [w2]
 
 
 # TODO: construct 5 random sentences

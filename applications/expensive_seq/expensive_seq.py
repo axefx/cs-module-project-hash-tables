349f16cea3bb6a0e3409899911125fa12e16294b
Axel <axacorro@gmail.com>
2020-07-08 22:00:29 -0700

    implemented expensive_seq with a dict as cache

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,8 +1,16 @@
-# Your code here
 
-
+cache = {}
 def expensive_seq(x, y, z):
-    # Your code here
+    i = (x,y,z)
+    # basecase
+    if x <= 0:
+        return y + z
+    else:
+        if i not in cache:
+            cache[i] = expensive_seq(x-1,y+1,z) + expensive_seq(x-2,y+2,z*2) + expensive_seq(x-3,y+3,z*3)
+            return cache[i]
+        else:
+            return cache[i]
 
 
 

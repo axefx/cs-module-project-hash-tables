867d94ddd5a892c43e30e88fd871d5c720eb7151
Axel <axacorro@gmail.com>
2020-07-09 22:05:15 -0700

    added create_sentence with word_map, initial mvp

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -20,4 +20,27 @@ for w1,w2 in word_pairs:
 
 # TODO: construct 5 random sentences
 # Your code here
+def get_start_words(words_list):
+    return [word for word in words_list if word[0].isupper() or (word[0] == '"' and word[1].isupper())]
 
+def is_stop_word(word):
+    ends = ['.', '?', '!']
+    return word[-1] in ends or (word[-1] == '"' and word[-2] in ends)
+
+def get_next_words(word,word_map):
+    return word_map[word]
+
+
+def create_sentence(word_map):
+    rand_start_word = random.choice(get_start_words(words_list))
+    next_word = random.choice(get_next_words(rand_start_word,word_map))
+    sentence = [rand_start_word]
+    while not is_stop_word(next_word):
+        sentence.append(next_word)
+        next_word = random.choice(get_next_words(next_word,word_map))
+    sentence.append(next_word)
+    return ' '.join(sentence)
+
+for i in range(5):
+    sentence = create_sentence(word_map)
+    print(sentence)
